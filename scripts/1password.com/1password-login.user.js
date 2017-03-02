02e38ed2c5cdf7662db468032c7e722f6d81c1e1
laomo <laomo@lmbj.net>
2017-03-02 11:42:37 +0800

    Update 1password-login.user.js
    add qunar login

--- scripts/1password.com/1password-login.user.js
+++ scripts/1password.com/1password-login.user.js
@@ -5,18 +5,21 @@
 // @description Kill QRCode For 1Password Login
 // @include     https://login.taobao.com/*
 // @include     https://passport.jd.com/*
+// @include     https://user.qunar.com/passport/login.jsp
 // @license     MIT
 // @updateURL  	https://github.com/laomo/userscripts/raw/master/scripts/1password.com/1password-login.user.js
 // @downloadURL https://github.com/laomo/userscripts/raw/master/scripts/1password.com/1password-login.user.js
 // @homepage    https://github.com/laomo/userscripts
 // @require	    https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
-// @version		0.1
+// @version		0.2
 // ==/UserScript==
 
 setTimeout(function () {
    //alert($("div[class='login-tab login-tab-r']").attr('class'));
    //taobao
-   $("i[id=J_Quick2Static]").click();
+   $("#J_Quick2Static").click();
    //jd
    $("div[class='login-tab login-tab-r']").click();
+   //qunar
+   $(".qrlayer").css("visibility", "hidden");
 }, 1000);
